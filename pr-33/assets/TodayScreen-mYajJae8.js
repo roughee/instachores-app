b3aa65f0c5822093a8e1e11f5c28e48aa25c7065
roughee <roughee@users.noreply.github.com>
2026-09-08 18:58:35 +0000

    deploy: aee9bc634c2e09ea8c943744cf89d76c587bc55c

---
+++ pr-33/assets/TodayScreen-mYajJae8.js
@@ -0,0 +1,1 @@
+import{f as e,h as t,i as n,l as r,v as i}from"./index-BBJdcDyu.js";import{t as a}from"./EmptyState-OnoEq7LY.js";var o=e({__name:`TodayScreen`,setup(e){return(e,o)=>(t(),r(a,{icon:i(n),label:`See what happened today once someone logs a task.`},null,8,[`icon`]))}});export{o as default};
