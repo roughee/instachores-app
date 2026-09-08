be918d30795f388b7bd3dd2a4a454e03722bd370
roughee <roughee@users.noreply.github.com>
2026-09-08 19:24:23 +0000

    deploy: 4aaa8bd442470dafb7577e4b005c0ab233afa52b

---
+++ pr-36/assets/RewardsScreen-BounYA61.js
@@ -0,0 +1,1 @@
+import{_ as e,b as t,i as n,m as r,u as i}from"./index-CRHWUkBY.js";import{t as a}from"./EmptyState-C-r25nVd.js";var o=r({__name:`RewardsScreen`,setup(r){return(r,o)=>(e(),i(a,{icon:t(n),label:`Turn points into rest once rewards are set up.`},null,8,[`icon`]))}});export{o as default};
